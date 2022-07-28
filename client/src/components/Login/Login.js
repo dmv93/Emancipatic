3483279dd3a0fb070aca21ed1e8f220ec1f928a4
cosmicdreams02 <95249832+cosmicdreams02@users.noreply.github.com>
2022-07-28 19:57:30 +0200

    Login upload

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -18,6 +18,9 @@ const Login = () => {
     }
     return (
         <div>
+            <div className="accesoLogin">
+                <h3 id="h3_login">Login</h3>
+            </div>
             <div className="form_login">
                 <form className="formulario_login">
                     <label htmlFor="" id="label_login_telefono">Teléfono</label>
@@ -28,12 +31,10 @@ const Login = () => {
 
                     <label htmlFor="" id="label_login_privacidad"></label>
                     <input type={"checkbox"} id="input_login_privacidad" name="privacidad" placeholder="" onChange={(e) => { setPrivacidad(e.target.value) }} />He leido y acepto la política de privacidad
-
                     <Link to={"/"} ><input type={"button"} className="button_login_enviar" value={"ENVIAR"} /></Link>
-
                 </form>
-            </div>
         </div>
+            </div>
     )
 
 }
