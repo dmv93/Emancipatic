8896ad8f09da702553b43c97744b5ea788c0233b
cosmicdreams02 <95249832+cosmicdreams02@users.noreply.github.com>
2022-07-30 18:21:18 +0200

    Perfil tick

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,10 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar () {
+
+
+
+    const [atras, setAtras] = useState("");
+    const [menu, setMenu] = useState("");
+
     return(
         <div className="nav_nav">
             <div className="navbar">
-                
+            <input type={"button"} className="button_navbar_" onClick={() => setAtras()} value={"ATRÁS"} />
+            <input type={"button"} className="button_login_enviar" onClick={() => setMenu()} value={"MENÚ"} />
             </div>
         </div>
     )
